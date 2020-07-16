f164f446dd029ed8d84c34fc4e606918d2c75457
lhuang <2539203552@qq.com>
2020-07-16 22:51:29 +0800

    feat:谷歌hash方法；

--- jwt/src/main/java/com/lhuang/blog/user/gateway/web/IndexController.java
+++ jwt/src/main/java/com/lhuang/blog/user/gateway/web/IndexController.java
@@ -1,8 +1,15 @@
 package com.lhuang.blog.user.gateway.web;
 
 
+import com.google.common.hash.Hashing;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 @RequestMapping("/index")
@@ -17,6 +24,30 @@ public class IndexController {
     public String register(){
         return "register";
     }
+
+    @RequestMapping("/monitor")
+    @Transactional
+    public String monitor(){
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+
+        long l = Hashing.murmur3_32().hashString("asikfiuiwueriwueriejarfkls", Charset.defaultCharset()).asInt();
+        System.out.println("生成的hash值:"+l);
+        long l1 = Hashing.murmur3_32().hashString("asikfiuiwueriwueriejarfkls", Charset.defaultCharset()).asInt();
+        System.out.println("生成的hash值:"+l1);
+
+    }
+
 
 
 }
--- jwt/src/main/java/com/lhuang/blog/user/rocketmq/RocketmqConsumer.java
+++ jwt/src/main/java/com/lhuang/blog/user/rocketmq/RocketmqConsumer.java
@@ -1,5 +1,6 @@
 package com.lhuang.blog.user.rocketmq;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.consumer.DefaultMQPullConsumer;
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
@@ -12,10 +13,17 @@ import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.apache.rocketmq.spring.core.RocketMQPushConsumerLifecycleListener;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.omg.CORBA.TIMEOUT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 /**
  * @author lhunag
@@ -51,4 +59,44 @@ public class RocketmqConsumer implements RocketMQListener<String>, RocketMQPushC
     public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
         return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
     }
+
+    public static void main(String[] args) throws InterruptedException {
+        ConcurrentHashMap<Long, String> collect = LongStream.rangeClosed(1, 10).boxed()
+                .collect(Collectors.toConcurrentMap(i -> i, v -> UUID.randomUUID().toString(), (o1, o2) -> o1, ConcurrentHashMap::new));
+
+        LongStream.rangeClosed(1, 10).boxed().collect(Collectors.toConcurrentMap(k -> k, v -> new LongAdder(),
+                (v1, v2) -> v1, ConcurrentHashMap::new)).computeIfAbsent(1L,v->new LongAdder()).increment();
+
+        int i = ThreadLocalRandom.current().nextInt(100);
+        System.out.println(i);
+        System.out.println(collect);
+        ForkJoinPool forkJoinPool = new ForkJoinPool(10);
+
+        forkJoinPool.execute(()->{
+            IntStream.rangeClosed(1,10).parallel().forEach(System.out::println);
+
+        });
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.SECONDS);
+
+
+        /**
+         * CopyOnWriteArrayList 虽然是一个线程安全的 ArrayList，但因为其实现方式是，每次
+         * 修改数据时都会复制一份数据出来，所以有明显的适用场景，即读多写少或者说希望无锁读
+         * 的场景。
+         */
+
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
+                ()-> System.out.println("每秒打印1次"),0,1, TimeUnit.SECONDS);
+
+
+        new ThreadFactoryBuilder().build();
+
+        //如果要配置 Feign 的读取超时，就必须同时配置连接超时，才能生效
+        //同时配置 Feign 和 Ribbon 的超时，以 Feign 为准
+        //Ribbon Get 请求自动重试1次
+    }
+
+
+
 }
